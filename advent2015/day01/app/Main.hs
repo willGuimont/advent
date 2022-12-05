ad4dbd50ac84632177ccc330026fa6fdc8e128d7
William Guimont-Martin <wil9876543210@hotmail.com>
2022-12-05 17:43:07 -0500

    2015 day 1

---
+++ advent2015/day01/app/Main.hs
@@ -0,0 +1,14 @@
+module Main (main) where
+
+import Data.List (elemIndex)
+
+toLevelChange :: Char -> Int
+toLevelChange '(' = 1
+toLevelChange ')' = -1
+toLevelChange _ = 0
+
+main :: IO ()
+main = do
+  input <- readFile "data/input.txt"
+  print . sum . map toLevelChange $ input
+  print . fmap (+ 1) . elemIndex (-1) . scanl1 (+) $ toLevelChange <$> input
