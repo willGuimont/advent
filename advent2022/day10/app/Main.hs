3b93364be6e02133808c4f4120a41dd1c9251f96
William Guimont-Martin <wil9876543210@hotmail.com>
2022-12-10 10:14:52 -0500

    2022 day 10 part 1

---
+++ advent2022/day10/app/Main.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Main (main) where
+
+import Control.Lens
+import Control.Monad.Except (throwError)
+import Text.ParserCombinators.Parsec
+
+-- Types
+data Instruction
+  = AddX Int
+  | NoOp
+  deriving (Show)
+
+-- Parsing
+parseSignedInt :: Parser Int
+parseSignedInt = char '-' >> negate . read <$> many1 digit
+
+parseInt :: Parser Int
+parseInt = read <$> many1 digit
+
+parseAddX :: Parser [Instruction]
+parseAddX = string "addx " >> (\v -> [NoOp, AddX v]) <$> (parseSignedInt <|> parseInt)
+
+parseNoOp :: Parser [Instruction]
+parseNoOp = string "noop" >> return [NoOp]
+
+parseInstruction :: Parser [Instruction]
+parseInstruction = parseAddX <|> parseNoOp
+
+parseProgram :: Parser [Instruction]
+parseProgram = concat <$> parseInstruction `sepBy` newline
+
+parseInput :: String -> Either String [Instruction]
+parseInput s = case parse parseProgram "program" s of
+  Left err -> throwError $ show err
+  Right x -> return x
+
+-- Logic
+data Computer = Computer
+  { _regX :: Int,
+    _cycles :: Int
+  }
+  deriving (Show)
+
+makeLenses ''Computer
+
+waitCycles :: Int -> Computer -> Computer
+waitCycles v = over cycles (+ v)
+
+addRegisterX :: Int -> Computer -> Computer
+addRegisterX v = over regX (+ v)
+
+execInstruction :: Instruction -> Computer -> Computer
+execInstruction (AddX v) = addRegisterX v . waitCycles 1
+execInstruction NoOp = waitCycles 1
+
+signalStrength :: Computer -> Int
+signalStrength c = view cycles c * view regX c
+
+main :: IO ()
+main = do
+  input <- readFile "data/input.txt"
+  let (Right program) = parseInput input
+      initComputer = Computer 1 1
+      states = scanl (flip execInstruction) initComputer program
+      signal = filter (\s -> let n = view cycles s in (n - 20) `mod` 40 == 0) states
+  print $ signalStrength <$> signal
+  print . sum $ signalStrength <$> signal
