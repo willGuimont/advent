b4550c4eeae77fbb402a1c2c31065fed4819b11f
William Guimont-Martin <wil9876543210@hotmail.com>
2024-12-07 10:15:57 -0500

    day 7

---
+++ app/AoC2024/Day07.hs
@@ -0,0 +1,67 @@
+module AoC2024.Day07 (partOne, partTwo) where
+
+import Text.ParserCombinators.Parsec
+
+-- Types
+data Equation = Equation Int [Int] deriving (Eq, Show)
+
+-- Parsing
+number :: Parser Int
+number = read <$> many1 digit
+
+equation :: Parser Equation
+equation = do
+  test <- number
+  _ <- string ": "
+  ns <- number `sepBy` char ' '
+  return $ Equation test ns
+
+equations :: Parser [Equation]
+equations = equation `sepBy` newline
+
+parseInput :: String -> Either String [Equation]
+parseInput s = case parse equations "equations" s of
+  Left err -> Left $ show err
+  Right res -> Right res
+
+-- Logic
+cartesianPower :: [a] -> Int -> [[a]]
+cartesianPower _ 0 = [[]]
+cartesianPower xs n = [x : ys | x <- xs, ys <- cartesianPower xs (n - 1)]
+
+applyEq :: [Int -> Int -> Int] -> [Int] -> Int
+applyEq _ [] = 0
+applyEq _ [n] = n
+applyEq (op:ops) (x:y:xs) = applyEq ops (x `op` y : xs)
+applyEq _ (x:_) = x
+
+isValid :: [Int -> Int -> Int] -> Equation -> Bool
+isValid ops (Equation test ns) = any (\ops -> test == applyEq ops ns) opss
+  where
+    n = length ns - 1
+    opss :: [[Int -> Int -> Int]]
+    opss = cartesianPower ops n
+
+getTest :: Equation -> Int
+getTest (Equation test _) = test
+
+concatOp :: Int -> Int -> Int
+concatOp x y = read $ show x <> show y
+
+-- Parts
+partOne :: String -> IO ()
+partOne input = do
+  putStrLn "- Part One -"
+
+  let (Right eqs) = parseInput $ init input
+  let result = sum $ getTest <$> filter (isValid [(+), (*)]) eqs
+  print result
+
+partTwo :: String -> IO ()
+partTwo input = do
+  putStrLn "- Part Two -"
+
+  let (Right eqs) = parseInput $ init input
+  let result = sum $ getTest <$> filter (isValid [(+), (*), concatOp]) eqs
+  print result
+
--- app/Main.hs
+++ app/Main.hs
@@ -6,26 +6,25 @@ import System.Environment (getEnv)
 import Data.Text (unpack)
 import Advent
 
-import AoC2024.Day06
+import AoC2024.Day07
 
 -- Changes daily
 year :: Integer
 year = 2024
 
 day :: Integer
-day = 6
+day = 7
 
 example :: String
-example = "....#.....\n\
-\.........#\n\
-\..........\n\
-\..#.......\n\
-\.......#..\n\
-\..........\n\
-\.#..^.....\n\
-\........#.\n\
-\#.........\n\
-\......#...\n"
+example = "190: 10 19\n\
+\3267: 81 40 27\n\
+\83: 17 5\n\
+\156: 15 6\n\
+\7290: 6 8 6 15\n\
+\161011: 16 10 13\n\
+\192: 17 8 14\n\
+\21037: 9 7 18 13\n\
+\292: 11 6 16 20\n"
 
 -- AoC wrapper
 getSession :: IO String
